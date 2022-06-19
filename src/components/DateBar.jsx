fd7e02d0987456ae6aef2032b83053a8b447c052
Sachet Thapa <68702000+supersmarty07@users.noreply.github.com>
2022-06-19 22:35:32 +1000

    Update DateBar.jsx

--- src/components/DateBar.jsx
+++ src/components/DateBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
-//carousel
+//in this component, letf/right button click is read, the button icon comes from react-icon library and therefore button clicks
+//send the value to the parent which in turn filters the data by date
 const DateBar = (props) => {
   let leftMove = props.leftMove;
   let dateNow = props.dateNow.week;
